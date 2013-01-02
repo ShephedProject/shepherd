c07c18e4868f2340e51b0544488d235ec6131821
Max Barry <mythtv@maxbarry.com>
2013-01-02 22:55:39 +0000

    Added util/chanscan-foxtel.pl, for channel checking Pay TV
    git-svn-id: file:///home/lex/Downloads/shepdump/svn/trunk@1435 12876d61-f61e-0410-a8eb-f2f112c5ae1a

---
+++ util/chanscan-foxtel.pl
@@ -0,0 +1,179 @@
+#!/usr/bin/perl
+
+use strict;
+
+use LWP::Simple;
+use Data::Dumper;
+use HTML::TreeBuilder;
+
+use lib 'references';
+use lib '../references';
+
+use Shepherd::Common;
+
+my $region_channels;
+my $ua;
+my $DATASOURCE = 'http://www.yourtv.com.au';
+my $URL_FTA = "$DATASOURCE/guide/ajax/channels/ChannelsFta.aspx?region_id=";
+my $URL_PAY = "$DATASOURCE/guide/ajax/channels/ChannelsPay.aspx?region_id=";
+my $REGIONIDS_PAY = { 'Foxtel' => 168 };
+my $DATASOURCE2 = 'http://au.tv.yahoo.com/tv-guide/';
+
+&read_channels_list;
+
+&setup_ua;
+
+print "Checking online guide...\n\n";
+print "Channels found to be not in the official list should almost always\n".
+      "be added. Official channels not found online should probably be left\n".
+      "alone, as that simply means the channel was not available in this\n".
+      "particular datasource--it might be available via a different grabber.\n\n";
+
+print Dumper($region_channels); 
+foreach my $region (sort { $a <=> $b } keys %$region_channels)
+{
+    printf "Region %s: ", $region;
+
+    my ($url, $url2);
+    if ($region =~ /^\d+$/)
+    {
+	$url = $URL_FTA . $region;
+	$url2 = $DATASOURCE2 . $region . '/0/';
+    }
+    elsif (defined $REGIONIDS_PAY->{$region})
+    {
+	$url = $URL_PAY . $REGIONIDS_PAY->{$region};
+	$url2 = $DATASOURCE2 . '73/168/';
+    }
+    else
+    {
+	print "!! Unknown\n";
+	next;
+    }
+
+    my $content = &Shepherd::Common::get_url($url);
+
+    my $tree = HTML::TreeBuilder->new_from_content($content);
+    my $h3 = $tree->look_down('_tag' => 'h3');
+    unless ($h3)
+    {
+	die "Couldn't find H3 in this:\n$content\n";
+    }
+    my $region_name = $h3->as_text();
+    print "$region_name\n";
+
+    ########################
+    # For debugging
+    print "Channels:\n" . join("\n", sort @{$region_channels->{$region}}) . ".\n";
+    ########################
+
+    my @yourtv_channels;
+    foreach my $tag ($tree->look_down('_tag' => 'label'))
+    {
+	next if ($tag->attr('for') eq 'select-all-fta');
+	my $chan = $tag->as_text();
+	$chan =~ s/^\s+//;
+	$chan =~ s/\(.*?\)//;
+	$chan =~ s/\s+$//;
+
+	next if (grep($chan eq $_, @yourtv_channels));
+
+	push @yourtv_channels, $chan;
+    }
+
+    my @yahoo_channels;
+
+    $content = &Shepherd::Common::get_url($url2);
+    $tree = HTML::TreeBuilder->new_from_content($content);
+    foreach my $tag ($tree->look_down('_tag' => 'li'))
+    {
+	next unless ($tag->attr('class') and $tag->attr('class') =~ /row channel/);
+	my $h3 = $tag->look_down('_tag' => 'h3');
+	my $chan = $h3->as_text();
+
+	###########################################
+	# For debugging
+	print "Chan: $chan.\n";
+	###########################################
+
+	push @yahoo_channels, $chan;
+    }
+
+    my @matched_channels;
+    foreach my $chan (@{$region_channels->{$region}})
+    {
+	if (grep($chan eq $_, @matched_channels))
+	{
+	    print " & \"$chan\": Duplicated in channel_list\n";
+	    next;
+	}
+	my @a = grep ($chan ne $_, @yourtv_channels);
+	my @b = grep($chan ne $_, @yahoo_channels);
+
+	if (@a == @yourtv_channels and @b == @yahoo_channels)
+	{
+	    print " ? \"$chan\" unknown to both YourTV and Yahoo\n";
+	}
+	elsif (@a == @yourtv_channels)
+	{
+	    print " ? \"$chan\" unknown to YourTV\n";
+	}
+	elsif (@b == @yahoo_channels)
+	{
+	    print " ? \"$chan\" unknown to Yahoo\n";
+	}
+	@yourtv_channels = @a;
+	@yahoo_channels = @b;
+	push @matched_channels, $chan;
+    }
+    foreach my $chan (@yourtv_channels)
+    {
+	print " ! \"$chan\" in YourTV but not channels_list.\n";
+    }
+
+    foreach my $chan (@yahoo_channels)
+    {
+	print " ! \"$chan\" in Yahoo but not channels_list.\n";
+    }
+
+    sleep 1;
+}
+
+
+print "Done.\n";
+
+
+
+sub setup_ua
+{
+  print "Refreshing UA.\n";
+
+  if ($ua)
+  {
+     print "Sleeping...\n";
+     sleep(5);
+  }
+
+  &Shepherd::Common::set_default('debug', 3);
+  $ua = &Shepherd::Common::setup_ua( cookie_jar => 1 );
+}
+
+sub read_channels_list
+{
+    my $fn = 'references/channel_list/channel_list';
+    unless (open (FN, 'channel_list') or open (FN, 'references/channel_list') or open(FN, '../references/channel_list'))
+    {
+	print "ERROR: Unable to open references/channel_list: $!!\n";
+	return;
+    }
+    while (my $line = <FN>)
+    {
+	if ($line =~ /^(\w+):(.*)/)
+	{
+	    $region_channels->{$1} = [ split(/,/, $2) ];
+	}
+	last if ($line =~ /---migrate---/);
+    }
+    printf "Read in channels for %d regions.\n", scalar(keys(%$region_channels));
+}
+
